88827daed9e60c45fa649887511f5f09b95fb05d
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-11-10 10:13:28 +0100

    GH-2: Fix condition using SKU/kind check

--- r-storage-account.tf
+++ r-storage-account.tf
@@ -78,7 +78,7 @@ resource "azurerm_storage_account" "storage" {
   }
 
   dynamic "queue_properties" {
-    for_each = var.queue_properties_logging != null && try(length(var.queues), 0) > 0 ? ["enabled"] : []
+    for_each = var.queue_properties_logging != null && contains(["Storage", "StorageV2"], var.account_kind) ? ["enabled"] : []
     content {
       logging {
         delete                = var.queue_properties_logging.delete
