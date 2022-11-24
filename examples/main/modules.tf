7b56b402a4c2e152685b5c60acd80a56f7568dec
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-11-24 10:40:03 +0100

    AZ-891: Update example (drop inter-dependency with run-common module)

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -15,17 +15,17 @@ module "rg" {
   stack       = var.stack
 }
 
-module "logs" {
-  source  = "claranet/run-common/azurerm//modules/logs"
-  version = "x.x.x"
+# module "logs" {
+#   source  = "claranet/run-common/azurerm//modules/logs"
+#   version = "x.x.x"
 
-  client_name         = var.client_name
-  environment         = var.environment
-  stack               = var.stack
-  location            = module.azure_region.location
-  location_short      = module.azure_region.location_short
-  resource_group_name = module.rg.resource_group_name
-}
+#   client_name         = var.client_name
+#   environment         = var.environment
+#   stack               = var.stack
+#   location            = module.azure_region.location
+#   location_short      = module.azure_region.location_short
+#   resource_group_name = module.rg.resource_group_name
+# }
 
 module "storage_account" {
   source  = "claranet/storage-account/azurerm"
@@ -59,8 +59,8 @@ module "storage_account" {
   }
 
   logs_destinations_ids = [
-    module.logs.logs_storage_account_id,
-    module.logs.log_analytics_workspace_id
+    # module.logs.logs_storage_account_id,
+    # module.logs.log_analytics_workspace_id
   ]
 
   # Set by default
