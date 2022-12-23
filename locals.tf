152150e2254756b181b95185161cfa46bdcd5950
Thierry Lei <thierry.lei-ext@fr.clara.net>
2022-12-23 11:18:27 +0100

    AZ-964: Apply suggestion

--- locals.tf
+++ locals.tf
@@ -3,7 +3,10 @@ locals {
   # > Small address ranges using "/31" or "/32" prefix sizes are not supported. These ranges should be configured using individual IP address rules without prefix specified.
   storage_ip_rules = toset(flatten([for cidr in var.allowed_cidrs : (length(regexall("/3.", cidr)) > 0 ? [cidrhost(cidr, 0), cidrhost(cidr, -1)] : [cidr])]))
 
-  pitr_enabled = alltrue([
-    for k, v in var.storage_blob_data_protection : try(tobool(v), k == "delete_retention_policy_in_days" ? v > 0 : v > 2)
-  ]) && !var.nfsv3_enabled
+  pitr_enabled = (
+    alltrue([var.storage_blob_data_protection.change_feed_enabled, var.storage_blob_data_protection.versioning_enabled, var.storage_blob_data_protection.container_point_in_time_restore])
+    && var.storage_blob_data_protection.delete_retention_policy_in_days > 0
+    && var.storage_blob_data_protection.container_delete_retention_policy_in_days > 2
+    && !var.nfsv3_enabled
+  )
 }
