49aa3c69f7e551fcd0896b3077f82757093d6998
Jérôme Respaut <jerome.respaut@claranet.com>
2024-08-12 10:31:57 +0200

    fix(GH-17): deprecated `enable_https_traffic_only` as been superseded by `https_traffic_only_enabled`
    https://github.com/claranet/terraform-azurerm-storage-account/issues/17

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.102"
+      version = "~> 3.114"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
