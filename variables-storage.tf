fdcd71af23fee102ce22f7d8a60fe669e6784deb
Jérémy MARMOL <jeremy.marmol@claranet.com>
2025-03-21 08:41:02 +0100

    chore(AZ-1542): fix validation condition and message

--- variables-storage.tf
+++ variables-storage.tf
@@ -242,7 +242,7 @@ variable "allowed_copy_scope" {
   type        = string
   default     = null
   validation {
-    condition     = var.allowed_copy_scope == null || (var.allowed_copy_scope != null && try(contains(["AAD", "PrivateLink"], var.allowed_copy_scope), false))
-    error_message = "Allowed values for allowed_copy_scope are 'AAD' or 'PrivateLink'."
+    condition     = var.allowed_copy_scope == null || try(contains(["AAD", "PrivateLink"], var.allowed_copy_scope), false)
+    error_message = "Allowed values for allowed_copy_scope are `AAD` or `PrivateLink`."
   }
 }
