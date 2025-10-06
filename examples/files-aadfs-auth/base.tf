1ef53eaf8261fa9fccc2c5e88babb108a16c4f0f
Jérôme Respaut <jerome.respaut@claranet.com>
2025-10-06 18:00:47 +0200

    docs: 📚️ update module examples

---
+++ examples/files-aadfs-auth/base.tf
@@ -0,0 +1,21 @@
+data "http" "my_ip" {
+  url = "http://ip4.clara.net/?raw"
+}
+
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = "x.x.x"
+
+  azure_region = var.azure_region
+}
+
+module "rg" {
+  source  = "claranet/rg/azurerm"
+  version = "x.x.x"
+
+  client_name    = var.client_name
+  environment    = var.environment
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  stack          = var.stack
+}
--- examples/multiple-sa/modules.tf
+++ examples/multiple-sa/modules.tf
@@ -1,21 +1,3 @@
-module "azure_region" {
-  source  = "claranet/regions/azurerm"
-  version = "x.x.x"
-
-  azure_region = var.azure_region
-}
-
-module "rg" {
-  source  = "claranet/rg/azurerm"
-  version = "x.x.x"
-
-  client_name    = var.client_name
-  environment    = var.environment
-  location       = module.azure_region.location
-  location_short = module.azure_region.location_short
-  stack          = var.stack
-}
-
 locals {
   sa_list = {
     app1 = ["containerA", "containerB", ]
